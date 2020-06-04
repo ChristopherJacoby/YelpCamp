4b27e642652745743daa33bfcc19b6f1b630996d
christopher jacoby <christopher.a.jacoby@gmail.com>
2020-06-04 13:42:56 -0400

    added mongolab db

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const   commentRoutes       = require("./routes/comments"),
         campgroundsRoutes   = require("./routes/campgrounds"),
         indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_2", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp_2", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://dbUser:dbUser1234@cluster0-ywqpx.mongodb.net/yelpcampdb?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
